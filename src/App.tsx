88cb6021ca0cb05118d110cd40947e5b584eb3f1
rajib010 <pokhrelrajib016@gmail.com>
2025-07-06 20:05:43 +0545

    add: subtitles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-
 function App() {
   return (
-    <div>App</div>
-  )
+    <div className="container">
+      <video
+        src="https://ik.imagekit.io/ofpyxioc6c/cr.mp4?updatedAt=1751810331094?tr=w-800"
+        height="400"
+        width="800"
+        controls
+        autoPlay
+        muted
+        loop
+      >
+        <track kind="subtitles" src="/english.vtt" srcLang="en" label="English" default />
+        <track kind="subtitles" src="/nepali.vtt" srcLang="ne" label="Nepali"  />
+      </video>
+    </div>
+  );
 }
 
-export default App
+export default App;
